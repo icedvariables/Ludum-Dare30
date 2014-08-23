1d91c1984f0776694f677efa86b778e9420f209a
icedvariables <macodeblacko@gmail.com>
2014-08-23 06:49:51 +0100

    Added background and player
    Added a background and player texture and wrote code to display them.

--- states/GameState.js
+++ states/GameState.js
@@ -3,11 +3,25 @@ var GameState = function(){
 
 GameState.prototype = {
     preload:function(){
-        this.load.image("sprite", "res/banana.png");
+        game.load.spritesheet("player", "res/player.png", 64, 64);
+        game.load.image("background", "res/background.png");
     },
     
     create:function(){
-        this.sprite = this.add.sprite(0, 0, "sprite");
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        var background = this.add.sprite(0, 0, "background");
+        background.immovable = true;
+    
+        this.player = this.add.sprite(0, 0, "player");
+        this.player.scale.setTo(2, 2);
+        game.physics.arcade.enable(this.player);
+        this.player.collideWorldBounds = true;
+        this.player.animations.add("left", [0, 1, 2], 10, true);
+        this.player.animations.add("still", [3], true);
+        this.player.animations.add("right", [4, 5, 6], 10, true);
+        
+        this.player.play("still");
     },
     
     update:function(){
